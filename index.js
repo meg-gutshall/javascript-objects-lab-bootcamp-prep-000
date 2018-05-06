68aae7e4b929854d11794822ecee5ec69fe52bb8
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-05-06 15:34:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value) {
   recipes.prop = 1,
   recipes.prop2 = 2
+<<<<<<< HEAD
   
+=======
+>>>>>>> 7cc12995c98bef603e79e6e33ef8bf2a5fbe4e21
   return recipes
 }
 
@@ -14,6 +17,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
+<<<<<<< HEAD
   var clone = Object.assign({}, object);
   delete clone[key];
   return clone;
@@ -23,3 +27,11 @@ function destructivelyDeleteFromObjectByKey(object, key) {
   var clone2 = delete object[key];
   return clone2;
 }
+=======
+  var obj = {prop: 1}
+  var newObj = deleteFromObjectByKey(obj, 'prop')
+  
+  return newObj
+  return obj
+}
+>>>>>>> 7cc12995c98bef603e79e6e33ef8bf2a5fbe4e21
